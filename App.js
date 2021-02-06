ec9290bb8ce7e1ec0b7ae2a8c8ac958d4655682f
cappycap <bullara2@wwu.edu>
2021-02-06 11:45:28 -0800

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const colors = {
+  emerald: '#2ecc71',
+  forest: '#27ae60',
+  clouds: '#ecf0f1',
+  blueGray: '#344150',
+  darkGray: '#23272a',
+  black: '#000000',
+  white: '#ffffff'
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+// Splash screen for checking user status on app load.
+class SplashScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      refreshing : false
+    };
+  }
+
+  render() {
+
+    return (<View style={styles.container}>
+      <Text>Splash Screen!</Text>
+    </View>);
+  }
+
+}
+
+// Main class for app. Responsible for rendering app container.
+export default class AppContainer extends React.Component {
+
+  // Main rendering function. Always begins on the SplashScreen.
+  // Note: The Login and Privacy screens have been added to the Stack Navigator.
+  //        I found that React Navigation creates problems when trying to pass along state.
+  render() {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator headerMode='none' initialRouteName='Splash'>
+            <Stack.Screen name='Splash' component={SplashScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    );
+  }
+
+};
